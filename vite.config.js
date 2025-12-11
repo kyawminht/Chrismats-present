1dda60f1e4de87e1ccd53f75b066a01eafc9f91e
Kyaw Min Htwe <kyawminhtway288@gmail.com>
2025-12-11 08:39:04 +0630

    fix routing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ADD THIS BASE CONFIGURATION
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      // This helps with routing issues
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
